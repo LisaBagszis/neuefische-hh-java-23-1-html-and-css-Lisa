a5a09813c787c7637998992aa7f161816b42988d
LisaBagszis <lisa.bagszis@gmail.com>
2023-03-21 14:48:54 +0100

    Aufgabe gelöst

--- ts-basics.js
+++ ts-basics.js
@@ -14,20 +14,16 @@ console.log(add(4 , 5))*/
 var students = [
     { name: "Jens", id: "1" },
     { name: "Lisa", id: "2" },
-    { name: "Susanne", id: "3" }
+    { name: "Susanne", id: "3" },
 ];
 var Id = "1";
 function findStudentAndUpdateName(student) {
     if (Id == student.id) {
-        return student.name = "Mario";
+        return { name: "Mario", id: Id };
     }
+    return student;
 }
-var newStudents = students.filter(findStudentAndUpdateName);
+var newStudents = students.map(findStudentAndUpdateName);
+
+console.log(students);
 console.log(newStudents);
-function findStudentAndGiveNewStudent(student1) {
-    if (Id == student1.id) {
-        return student1.name = "Henry";
-    }
-}
-var newStudent = students.filter(findStudentAndGiveNewStudent);
-console.log(newStudent);
--- ts-basics.ts
+++ ts-basics.ts
@@ -12,33 +12,26 @@ function add( a : number, b : number) : number {
 }
 console.log(add(4 , 5))*/
 
-const students: {name: string, id: string}[] =
+const students =
     [
         {name: "Jens", id: "1"},
         {name: "Lisa", id: "2"},
-        {name: "Susanne", id: "3"}
-    ]
+        {name: "Susanne", id: "3"},
+    ];
 
-
-const Id: string = "1"
-function findStudentAndUpdateName(student: {name: string, id: string}) {
-    if(Id == student.id) {
-        return student.name = "Mario"
+const Id = "1";
+function findStudentAndUpdateName(student) {
+    if (Id == student.id) {
+        return { name: "Mario", id: Id};
     }
+    return student
 }
+const newStudents = students.map(findStudentAndUpdateName);
+console.log(students);
+console.log(newStudents);
 
-const newStudents: {name: string, id: string}[] = students.filter(findStudentAndUpdateName)
 
-console.log(newStudents)
 
-function findStudentAndGiveNewStudent(student1: {name: string, id: string}) {
-    if(Id == student1.id) {
-        return student1.name = "Henry"
-    }
-}
-
-const newStudent: {name: string, id: string}[] = students.filter(findStudentAndGiveNewStudent)
-console.log(newStudent)
 
 
 
